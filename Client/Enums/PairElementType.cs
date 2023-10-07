70d83f73bc4dd00e5de6182e2301ad1facc254a1
Guy Rahamim <guy.danino5@gmail.com>
2023-10-07 23:16:46 +0300

    add text and image support

--- Client/Enums/PairElementType.cs
+++ Client/Enums/PairElementType.cs
@@ -1,5 +1,14 @@
-public enum PairElementType
+public enum ContentType
 {
     Image,
     Text
+}
+
+public static class ContentTypeExtensions
+{
+    public static ContentType Random()
+    {
+        Random rand = new Random();
+        return rand.Next(2) == 0 ? ContentType.Image : ContentType.Text;
+    }
 }
